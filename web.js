54b63e94cd3add44dc123d63818478b4a9f0626d
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-07-04 14:03:02 +0200

    Create basic server and API. Add front-end that sends a request to the server.

--- web.js
+++ web.js
@@ -5,11 +5,42 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+
+app.use(express.static(__dirname + '/app'));
+
+
+
+// API Routes
+
+var apiPrefix = '/api/1';
+
+app.get(apiPrefix + '/hello', function(req, res) {
+  res.send('Gonna be an API here soon enough.');
 });
+
+
+
+// Start listening
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+
+// Database
+
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
